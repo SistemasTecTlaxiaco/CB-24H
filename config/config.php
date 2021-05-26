ed0c8361a95e82d2b92a6fd4194777e80c1631fe
Elvis <72048969+ElvisS10@users.noreply.github.com>
2021-05-26 03:43:42 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
-    define('URL', 'http://localhost/MVC_CB/');
-    define('HOST','localhost');
-    define('DB', 'usuarios');
-    define('USER','root');
-    define('PASSWORD','');
+    define('URL', 'https://coffe-bar.herokuapp.com/');
+    define('HOST','tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+    define('DB', 'u5krzwc18as2l5qn');
+    define('USER','lec71u8i4p1i6dx0');
+    define('PASSWORD','iwmfywj87xe1g57q');
     define('CHARSET', 'utf8mb4');
 ?>
