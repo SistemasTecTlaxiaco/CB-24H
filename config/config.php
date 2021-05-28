07810a99fa496c8dd4a1bfea2deb6a3b694e4f0f
Elvis <72048969+ElvisS10@users.noreply.github.com>
2021-05-28 00:32:20 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-    define('URL', 'https://coffe-bar.herokuapp.com/');
-    define('HOST','tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+    define('URL', 'localhost');
+    define('HOST','usuarios');
     define('DB', 'u5krzwc18as2l5qn');
     define('USER','lec71u8i4p1i6dx0');
     define('PASSWORD','iwmfywj87xe1g57q');
