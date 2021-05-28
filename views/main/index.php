0d79988b2643767862b70673b84158c3cbba7cfb
Anna-Aparicio <80292858+Anna-Aparicio@users.noreply.github.com>
2021-05-28 00:18:39 -0500

    Actualización encabezado

--- views/main/index.php
+++ views/main/index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <link rel="stylesheet"  href="<?php echo constant('URL');?>public/css/default.css"> 
-    <title>CAFE-BAR TLAXIACO</title>
+    <title>CB-24H CAFE-BAR TLAXIACO</title>
 </head>
 <body>
 
