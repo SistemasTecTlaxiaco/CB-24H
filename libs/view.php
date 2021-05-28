63480825d00312d0e28deaceef4f69437fef2aeb
Elvis <72048969+ElvisS10@users.noreply.github.com>
2021-05-28 00:43:52 -0500

    Vista/Controlador

--- libs/view.php
+++ libs/view.php
@@ -7,6 +7,7 @@
         }
         function render($nombre)
         {
+            //definimos la ruta de las vistas, para que mande a llamar al archivo ya con una asignacion de carga de archivo .PHP
             require 'views/' .$nombre . '.php';
         }
     }
